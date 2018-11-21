c83092ed5a57829bf59efdfb233adf6b33d5946f
Tara Sabella <tarasabella@gmail.com>
2018-11-21 18:46:26 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,14 @@
 def oxford_comma(array)
   if array.length == 2
     return "#{array[0]} and #{array[1]}"
-    #peanut butter and jelly
+    #returns "peanut butter and jelly"
   elsif array.length > 2 
     array[-1].insert(0, "and ")
   end 
  array.join(", ")
   end 
+  
+  
 
    
   
